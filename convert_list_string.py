627733740b85a9c906049e202a32f03eaf15ca52
Paul LeRoy, CSM <paleroy@the281group.com>
2024-03-22 19:50:00 -0700

    first commit

---
+++ convert_list_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+""" this function takes a List  and 
+returns a single string with all the elements of 
+the list separated by a one space."""
+
+
+def convert_list_string(list: list):
+    conv_string = " ,".join(list)
+    return conv_string
